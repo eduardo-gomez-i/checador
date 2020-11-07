6e61e6e42acd05620bc3cc15610cfa8e855ea751
eduardo <gomez.i.eduardomanuel@gmail.com>
2020-11-07 15:59:44 -0600

    agregar codigo qr aun falta generar

--- conecta_bdd.php
+++ conecta_bdd.php
@@ -2,10 +2,10 @@
 date_default_timezone_set('America/Mexico_City');
 ini_set('display_errors', 1);
 
-$db1_host="localhost";
+$db1_host="192.168.1.81";
 $db1_name="checador";
-$db1_user="root";
-$db1_password="";
+$db1_user="remoto";
+$db1_password= "checador123";
 $conexion1 = mysqli_connect($db1_host, $db1_user, $db1_password);
 if (!$conexion1){
 	die ("NO SE PUDO CONECTAR A LA BASE DE DATOS: ". mysqli_error($conexion1));
